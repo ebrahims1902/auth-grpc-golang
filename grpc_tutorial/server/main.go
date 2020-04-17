5cadcf79b3e41ce54cc2ffedcc1e1e9960d85a9b
ebrahims1902 <ebrahims1902@gmail.com>
2020-04-17 23:17:41 +0530

    First commit in auth-grpc

---
+++ grpc_tutorial/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"grpc_tutorial/proto"
+	pb "grpc_tutorial/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedAddServiceServer
+}
+
+func (s *server) SinUp(ctx context.Context, in *pb.SinUpRequest) (*pb.SinUpResponse, error) {
+	log.Printf("Received: %v", in)
+	return &pb.SinUpResponse{Message: "Success"}, nil
+}
+
+func (s *server) SinIn(ctx context.Context, in *pb.SinInRequest) (*pb.SinInResponse, error) {
+	log.Println("Received:", in)
+	return &pb.SinInResponse{Message: "Successfully SinIn"}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(e)
+	}
+
+}
